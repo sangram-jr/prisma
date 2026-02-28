2412ad8ad11a7cb6ea4b3a3dddf7fe10d10fecde
sangram-jr <sangrambera58@gmail.com>
2026-02-28 22:45:55 +0530

    add /todos/:id to get user's todo based on their id

--- basic/src/index.ts
+++ basic/src/index.ts
@@ -14,6 +14,24 @@ app.get('/user',async (req,res)=>{
     res.json(user);
 })
 
+// Route to get a user's todos based on their ID
+app.get("/todos/:id", async (req, res) => {
+    // Extract user ID from the URL parameter
+    const id = req.params.id;
+    // Find the user with the given ID and include their todos, username, and password
+    const users = await prisma.user.findFirst({
+        where: {
+            id: parseInt(id) // Convert the ID from string to integer
+        },
+        select: {
+            todos: true,  // Fetch the user's todos
+            username: true, // Fetch the user's username
+        }
+    });
+    // Send the user data as a JSON response
+    res.json(users);
+})
+
 app.listen(3000,()=>{
     console.log("Listening on port 3000");
     
